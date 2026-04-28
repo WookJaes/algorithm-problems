c50314b3350790cb2fe1976fd6f5cfc0067fd432
WookJae Jeong <dnrwo09@gmail.com>
2026-04-28 09:50:17 +0900

    [level 1] Title: 시저 암호, Time: 2.12 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            
+            if (Character.isLowerCase(ch)) {
+                ch = (char) ('a' + (ch - 'a' + n) % 26);
+            } else if (Character.isUpperCase(ch)) {
+                ch = (char) ('A' + (ch - 'A' + n) % 26);
+            }
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
