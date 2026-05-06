cbc967e4ec878138578cc6e36eeb6d591c2e6292
WookJae Jeong <dnrwo09@gmail.com>
2026-05-06 09:44:58 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.47 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < answer.length; i++) {
+            char ch = s.charAt(i);
+            
+            if (map.containsKey(ch)) {
+                answer[i] = i - map.get(ch);
+            } else {
+                answer[i] = -1;
+            }
+            
+            map.put(ch, i);
+        }
+        
+        return answer;
+    }
+}
