d232dee1cb71d0bff2f27a8da1a1766c1c7f30bc
WookJae Jeong <dnrwo09@gmail.com>
2026-05-07 09:42:51 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 297.53 ms, Memory: 55.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder left = new StringBuilder();
+        
+        for (int i = 1; i < food.length; i++) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                left.append(i);
+            }
+        }
+        
+        String right = new StringBuilder(left).reverse().toString();
+        return left + "0" + right;
+    }
+}
