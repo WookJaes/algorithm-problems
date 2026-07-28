a62b13c9df9de800bdadf2ebe80660012a736499
WookJae Jeong <dnrwo09@gmail.com>
2026-07-28 09:45:31 +0900

    [level 2] Title: 더 맵게, Time: 668.57 ms, Memory: 139 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,23 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int s : scoville) {
+            pq.offer(s);
+        }
+        
+        int count = 0;
+        
+        while (pq.size() > 1 && pq.peek() < K) {
+            int first = pq.poll();
+            int second = pq.poll();
+            
+            pq.offer(first + second * 2);
+            count++;
+        }
+        
+        return pq.peek() >= K ? count : -1;
+    }
+}
