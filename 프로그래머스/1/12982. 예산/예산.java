d449a3ed00048e7f8f646fd315da1805205675b2
Wookjae Jeong <dnrwo09@gmail.com>
2026-09-03 20:33:21 +0900

    [level 1] Title: 예산, Time: 5.45 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        
+        int answer = 0;
+        
+        for (int cost : d) {
+            if (budget < cost) {
+                break;
+            }
+            
+            budget -= cost;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
